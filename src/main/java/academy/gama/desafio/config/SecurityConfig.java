ae6ad40f3ce54d04d73d48250afcd8f9344a03fa
Renan Costa Alencar <renan.costaalencar@gmail.com>
2021-02-12 16:26:52 -0300

    Configuração inicial do Spring Security

--- src/main/java/academy/gama/desafio/config/SecurityConfig.java
+++ src/main/java/academy/gama/desafio/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -21,12 +22,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private Environment env;
 
+	/***
+	 * Array com os endpoints liberação sem precisar de autenticação
+	 */
+	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
+
+	/***
+	 * Array com os endpoints liberação para recuperar dados sem autenticação
+	 */
+	private static final String[] PUBLIC_MATCHERS_GET = { "/logar/**" };
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
 			http.headers().frameOptions().disable();
 		}
-		
+
 		http.cors().and().csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		http.authorizeRequests().anyRequest().permitAll();
@@ -40,4 +51,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
 	}
+
+	/***
+	 * Método para autorizar acesso aos endpoints que precisam de autenticação, sem
+	 * configuração de ataque CSRF pois o sistema é stateless e sem criar seção de
+	 * usuário.
+	 */
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+//			http.headers().frameOptions().disable();
+//		}
+//		http.cors().and().csrf().disable();
+//		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+//				.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
+//		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//	}
+
+	/***
+	 * Método que configura as restrições de CORS com restrições básicas
+	 * 
+	 * @return
+	 */
+//	@Bean
+//	CorsConfigurationSource corsConfigurationSource() {
+//		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+//		return source;
+//	}
 }
