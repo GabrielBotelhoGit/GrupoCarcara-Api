fe5b9f13716807a6986f079d0b71ce4e4c689b30
Gabriel Carreiras Botelho <gabriel.botelho@tenti.com.br>
2021-02-17 10:52:07 -0300

    Correção de bug na UserDetailsService

--- src/main/java/academy/gama/desafio/service/UserDetailsServiceImpl.java
+++ src/main/java/academy/gama/desafio/service/UserDetailsServiceImpl.java
@@ -3,13 +3,13 @@
  */
 package academy.gama.desafio.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import academy.gama.desafio.model.Usuario;
-import academy.gama.desafio.repository.UsuarioRepository;
 import academy.gama.desafio.security.UserSS;
 
 /**
@@ -19,11 +19,12 @@ import academy.gama.desafio.security.UserSS;
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 	
-	private UsuarioRepository usuarioRepository;
+	@Autowired
+	UsuarioService usuarioService;
 
 	@Override
 	public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
-		Usuario usuario = usuarioRepository.getUsuarioWithLogin(login);
+		Usuario usuario = usuarioService.getUsuarioWithLogin(login);
 		
 		if (usuario == null) {
 			throw new UsernameNotFoundException(login);
