92f77a0222fb1609cd09e51dff3d82d9021ba708
Guilherme <guilherme.pcruz@hotmail.com>
2021-02-18 13:47:34 -0300

    ContaServiceTest first automated tests
    And some refactoring on other test classes

--- src/test/java/academy/gama/desafio/service/ContaServiceTest.java
+++ src/test/java/academy/gama/desafio/service/ContaServiceTest.java
@@ -1,5 +1,52 @@
 package academy.gama.desafio.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import academy.gama.desafio.dto.UsuarioDto;
+import academy.gama.desafio.model.Conta;
+
+@SpringBootTest
 public class ContaServiceTest {
+	
+	@TestConfiguration
+	static class ContaServiceTestConfiguration{
+		@Bean
+		public ContaService cs() {
+			return new ContaService();
+		}
+		@Bean
+		public UsuarioService us() {
+			return new UsuarioService();
+		}
+	}
+	
+	@Autowired
+	ContaService cs;
+	
+	@Autowired
+	UsuarioService us;
+	
+	@Test
+	public void retorna_lista_de_contas() {
+		/**
+		 * Este caso cobre as funções addContasIniciais, addPlanosDeContasIniciais e getContasWithLogin da classe ContaService
+		 */
+		UsuarioDto userDto = us.addUsuario(new UsuarioDto("12345678910", "mocklogin", "Zé Teste", "123456"));
+		List<Conta> listaContas = cs.getContasWithLogin(userDto.getLogin());
+		assertNotNull(listaContas);
+		for(Conta conta: listaContas) {
+			assertNotNull(conta);
+			assertEquals( conta.getUsuario().getLogin(), userDto.getLogin());
+		}
+	}
 
 }
--- src/test/java/academy/gama/desafio/service/UsuarioServiceTest.java
+++ src/test/java/academy/gama/desafio/service/UsuarioServiceTest.java
@@ -4,9 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 
-import org.junit.Rule;
 import org.junit.jupiter.api.Test;
-import org.junit.rules.ErrorCollector;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -51,9 +49,6 @@ class UsuarioServiceTest {
 		}
 
 	}
-	
-	@Rule
-	public ErrorCollector error = new ErrorCollector();
 	
 	@Autowired
 	UsuarioService usuarioService;
